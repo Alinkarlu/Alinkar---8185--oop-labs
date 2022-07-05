3b6e48302e5e36f3f865ecbec17d89d1028e9e00
Alinkarlu <108324514+Alinkarlu@users.noreply.github.com>
2022-07-05 10:41:23 +0700

    Lab1

--- Lab1/write_read_file.py
+++ Lab1/write_read_file.py
@@ -7,11 +7,11 @@ b = float(input("Enter a number: "))
 sum = float(a) + float(b)
 print("%.0f + %.0f = %.1f" % (a,b,a+b))
 
+print("Writing the file to numbers.txt")
 with open ("numbers.txt" , "w") as w:
     v = w.write(("%.0f + %.0f = %.1f" % (a,b,a+b)))
 
-
-
+print("Reading the file to numbers.txt")
 with open ("numbers.txt" , "r") as r:
  numbers = r.read()
  
