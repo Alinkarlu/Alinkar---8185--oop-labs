a734100c72a02dd3bd71f487e70699bf525f2c3a
Alinkarlu <108324514+Alinkarlu@users.noreply.github.com>
2022-07-17 17:20:47 +0700

    Lab2

---
+++ Lab2/find_vowels.py
@@ -0,0 +1,6 @@
+string = input("Enter string input: ")
+vowels = [each for each in string if each in "aeiouAEIOU"]
+print("Vowels in" ,string, "are" ,vowels)
+
+# Name = Alinkar Lu
+# Student ID = 643040818-5
