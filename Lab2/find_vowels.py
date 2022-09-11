4d5dd82279659de849a0d4d172ff95207c0440f9
Matus Yaowvasrisuwan <matus.y@kkumail.com>
2022-09-11 15:15:44 +0700

    Deleting files

--- Lab2/find_vowels.py
+++
@@ -1,6 +0,0 @@
-string = input("Enter string input: ")
-vowels = [each for each in string if each in "aeiouAEIOU"]
-print("Vowels in" ,string, "are" ,vowels)
-
-# Name = Alinkar Lu
-# Student ID = 643040818-5
