ba06d3306301f9130a7e03d4a386830214a9e57b
Matus Yaowvasrisuwan <matus.y@kkumail.com>
2022-09-17 16:42:55 +0700

    Rename

--- Alinkar-6430408185-lab6/8185-lab6-prob3.py
+++ Alinkar-6430408185-lab6/8185-lab6-prob3.py
@@ -5,11 +5,12 @@ Alinkar Lu
 
 import math
 
+
 class Circle:
 
     def __init__(self, radius):
         self.radius = radius
-    
+
     def compute_area(self):
         return math.pi * self.radius ** 2
 
@@ -24,9 +25,11 @@ class Circle:
                 return number
             except ValueError:
                 print(f"Please enter a valid decimal number")
-        
+
+
 if __name__ == '__main__':
-    radius = Circle.get_number("Enter radius:")   
+    radius = Circle.get_number("Enter radius:")
     circle = Circle(radius)
     print(f"The circle with radius {circle.radius}")
-    print(f"has the area as {circle.compute_area():0.2f} and the circumference as {circle.compute_circumference():0.2f}")
+    print(
+        f"has the area as {circle.compute_area():0.2f} and the circumference as {circle.compute_circumference():0.2f}")
